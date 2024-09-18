a54d0c85152b3a2b3f14e4dd6c367a3f61a09db7
Rahul Kumar <rg0562731@gmail.com>
2024-09-18 23:28:46 +0530

    added two more vendor classes

---
+++ HeadFirstDesignPattern/TheCommandPattern/VendorClasses/Sprinkler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPattern.TheCommandPattern.VendorClasses
+{
+    public class Sprinkler
+    {
+        public void WaterOn()
+        {
+            Console.WriteLine("Watering grass");
+        }
+        public void WaterOff() 
+        { 
+            Console.WriteLine("Sprinkler Off");
+        }
+    }
+}
---
+++ HeadFirstDesignPattern/TheCommandPattern/VendorClasses/TV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeadFirstDesignPattern.TheCommandPattern.VendorClasses
+{
+    public class TV
+    {
+        public void ON()
+        {
+            Console.WriteLine("Turning on the TV");
+        }
+        public void OFF() 
+        { 
+            Console.WriteLine("TV is turned off");
+        }
+
+    }
+}
